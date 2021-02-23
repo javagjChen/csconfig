add9b6af08a7e1552b667a440fa33e48461d50ed
chengj17 <chengj17@meicloud.com>
2021-02-23 16:53:42 +0800

    init

---
+++ src/main/java/com/cgj/csconfig/CsConfigApplication.java
@@ -0,0 +1,13 @@
+package com.cgj.csconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cgj/csconfig/CsconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgj.csconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
